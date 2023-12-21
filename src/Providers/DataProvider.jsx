dbef6c64e747ad0ecb90ac909483d7cb907b1f79
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2023-12-21 22:04:23 +0600

    update

--- src/Providers/DataProvider.jsx
+++ src/Providers/DataProvider.jsx
@@ -1,38 +1,55 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState, useMemo } from "react";
+import debounce from "lodash/debounce";
 
 export const DataContext = createContext({
   data: [],
   isLoading: true,
   error: null,
+  setSearchText: () => {},
 });
 
 export default function DataProvider({ children }) {
   const [data, setData] = useState([]);
-  const [searchText, setSearchText] = useState("Islam");
+  const [searchText, setSearchText] = useState("Programming");
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          `https://openlibrary.org/search.json?title=${searchText}`
-        );
-        if (!response) {
-          throw new Error("Network response was not ok");
+  const fetchData = useMemo(
+    () =>
+      debounce(async () => {
+        try {
+          const controller = new AbortController();
+          const signal = controller.signal;
+
+          const response = await fetch(
+            `https://openlibrary.org/search.json?title=${searchText}`,
+            { signal }
+          );
+
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+
+          const result = await response.json();
+          setData(result);
+        } catch (err) {
+          if (err.name === "AbortError") {
+            console.log("Fetch aborted: ", err.message);
+          } else {
+            setError(err);
+          }
+        } finally {
+          setIsLoading(false);
         }
+      }, 300),
+    [searchText]
+  );
 
-        const result = await response.json();
-        setData(result);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
+  useEffect(() => {
     fetchData();
-  }, [searchText]);
+
+    return () => fetchData.cancel();
+  }, [fetchData]);
 
   const contextValue = {
     data,
